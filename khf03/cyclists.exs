e2636196f5bb5b21f771ee95b5e361670130fdcf
akos <szmoleniczki.akos@edu.bme.hu>
2025-10-10 12:54:48 +0200

    working valid_candidate?() check

--- khf03/cyclists.exs
+++ khf03/cyclists.exs
@@ -1,85 +1,55 @@
 defmodule Khf3 do
+
   @moduledoc """
   Ciklikus számlisták
   @author "Egyetemi Hallgató <egy.hallg@edu.bme.hu>"
   @date   "2025-10-xx"
   """
-  # számsorozatok száma, n (1 < n)
-  @type count() :: integer()
-  # számsorozat hossza, m (1 <= m)
-  @type cycle() :: integer()
-  # listahossz, len (1 < len)
-  @type size() :: integer()
-  # listaelem értéke, val (0 <= val <= m)
-  @type value() :: integer()
-  # listaelem sorszáma, ix (1 <= ix <= len)
-  @type index() :: integer()
-  # listaelem indexe és értéke
-  @type index_value() :: {index(), value()}
+  @type count() :: integer() # számsorozatok száma, n (1 < n)
+  @type cycle() :: integer() # számsorozat hossza, m (1 <= m)
+  @type size()  :: integer() # listahossz, len (1 < len)
+  @type value() :: integer() # listaelem értéke, val (0 <= val <= m)
+  @type index() :: integer() # listaelem sorszáma, ix (1 <= ix <= len)
+  @type index_value() :: {index(), value()} # listaelem indexe és értéke
 
-  @spec cyclists({n :: count(), m :: cycle(), len :: size()}, constraints :: [index_value()]) ::
-          results :: [[value()]]
+  # @spec cyclists({n::count(), m::cycle(), len::size()}, constraints::[index_value()]) :: results::[[value()]]
   # results az összes olyan len hosszú lista listája, melyekben
   # * az 1-től m-ig tartó számsorozat – ebben a sorrendben, esetleg
   #   közbeszúrt 0-kal – n-szer ismétlődik,
   # * len-n*m számú helyen 0-k vannak,
   # * a constraints korlát-listában felsorolt indexű cellákban a megadott
   #   értékű elemek vannak.
-  def cyclists({n, m, len}, constraints) do
-    # IO.inspect(constraints)
-    # 0-tól kezdődő indexeléssé transzformálva
-    constraints = Map.new(constraints, fn {ix, val} -> {ix - 1, val} end)
-    # IO.inspect(constraints)
-    zeros_available = len - n * m
-    # 1..m -> (counter from 0 mod m) + 1
-    # available = for num <- 1..m, do: num
-    # IO.puts("len:")
-    # IO.inspect(len)
-    # cyclists({m, len}, constraints, start_ix, counter, zeros_count, ls_acc)
-    generate_lists({n, m, len}, constraints, zeros_available, 0, 0, [], [])
+  # def cyclists({n, m, len}, constraints)
+
+  def cycle_num(counter, m) do
+    cycle = div(counter, m) + 1
+    num = rem(counter, m) + 1
+    {cycle, num}
   end
 
-  defp possible_values(ix, num, constraints, zeros_available) do
+  def candidates(ix, zeros, constraints, counter, m) do
+    constraint = Map.get(constraints, ix)
+    {_, num} = cycle_num(counter, m)
     cond do
-      Map.has_key?(constraints, ix) -> [Map.get(constraints, ix)]
-      zeros_available > 0 -> [num, 0]
+      constraint -> [constraint]
+      zeros > 0 -> [num, 0]
       true -> [num]
     end
   end
 
-  # All zeros used and reached end of list -> done
-  defp generate_lists({n, m, len}, constraints, zeros_available, ix, counter, current_list, list_acc)
-    when ix == len and zeros_available == 0 do
-      IO.puts "added list to accumulator"
-      IO.inspect current_list
-      IO.puts "-----------------"
-      [Enum.reverse(current_list)|list_acc]
+  def valid_candidate?(cand, previous, ix, constraints, zeros, m) do
+    next_in_cycle? = cand - previous == 1 and cand != 0
+    new_cycle? = cand - previous == 1 - m and cand != 0
+    zero? = cand == 0
+    zeros? = zeros > 0
+    constraint? = Map.has_key?(constraints, ix)
+
+    cond do
+      constraint? && zero? -> zeros?
+      constraint? -> cand == Map.get(constraints, ix)
+      (next_in_cycle? or new_cycle?) and not zero? -> true
+      zero? -> zeros?
+      true -> false
     end
-  # Zeros remain but reached end of list -> invalid list
-  defp generate_lists({n, m, len}, constraints, zeros_available, ix, counter, current_list, list_acc)
-    when zeros_available < 0 or (ix == len and zeros_available != 0) do
-      IO.puts "invalid list"
-      IO.inspect current_list
-      IO.puts "-----------------"
-      list_acc
-  end
-  defp generate_lists({n, m, len}, constraints, zeros_available, ix, counter, current_list, list_acc) do
-    # Process.sleep(2000)
-    # IO.puts "ix:"
-    # IO.inspect ix
-    num = rem(counter, m) + 1 # repeating counter 1..m
-    possibilities = possible_values(ix, num, constraints, zeros_available)
-    # IO.puts "zeros available:"
-    # IO.inspect zeros_available
-    # IO.puts "possibilities:"
-    # IO.inspect possibilities
-    # IO.puts "current list:"
-    # IO.inspect current_list
-    Enum.reduce(possibilities, list_acc, fn val, acc ->
-      new_zeros_available = if val == 0, do: zeros_available - 1, else: zeros_available
-      new_counter = if val == 0, do: counter, else: counter + 1 # this is needed to keep the sequence correct when we use a zero
-      new_current_list = [val|current_list]
-      generate_lists({n, m, len}, constraints, new_zeros_available, ix + 1, new_counter, new_current_list, acc)
-    end)
   end
 end
