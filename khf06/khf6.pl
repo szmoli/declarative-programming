f5bc2e50c56045d126c9a7250549f6a5b8a4b663
akos <szmoleniczki.akos@edu.bme.hu>
2025-11-14 13:25:53 +0100

    szukites_nullas done

--- khf06/khf6.pl
+++ khf06/khf6.pl
@@ -14,8 +14,6 @@
 
 % :- type szukites        ---> sor(sorszam,ertek) ; oszl(oszlopszam,ertek) ; nem.
 
-% :- pred kizarasos_szukites(feladvany_leiro::in, t_matrix::in, t_matrix::out, szukites::out).
-
 % Tartomány felveheti-e E-t.
 felveheti(E, E) :-
     integer(E).
@@ -76,7 +74,7 @@ oszlop_keres(N, Mx, E, Z, OszlopIx, Vonal) :-
     !.
 
 % Első jó vonal.
-vonal_keres(N, M, Mx, SorIx, OszlopIx, Tipus, Vonal) :-
+vonal_keres(N, M, Mx, SorIx, OszlopIx, Tipus, E, Vonal) :-
     Z is N - M,
     between(0, M, E),
         (   sor_keres(N, Mx, E, Z, SorIx, Vonal)
@@ -88,6 +86,84 @@ vonal_keres(N, M, Mx, SorIx, OszlopIx, Tipus, Vonal) :-
         ),
     !.
     
+% Feladatban megadott szűkítések
+szukites(0, ElvartZ, SorIx, OszlopIx, Vonal, Tipus, MxBe, MxKi, Sz) :-
+    szukites_nullas(ElvartZ, SorIx, OszlopIx, Vonal, Tipus, MxBe, MxKi, Sz),
+    !.
+szukites(E, _ElvartZ, SorIx, OszlopIx, Vonal, Tipus, MxBe, MxKi, Sz) :-
+    E > 0,
+    szukites_pozitiv(E, SorIx, OszlopIx, Sor, Oszlop, MxBe, MxKi), 
+    !.   
 
-% Veszünk egy E értéket 0..M közül növekvő sorrendbe
-% Keressük az első sort vagy oszlopot, ami:
+szukites_pozitiv(E, SorIx, OszlopIx, Sor, Oszlop, MxBe, MxKi) :-
+    elhagy_kiveve(E, OszlopIx, Sor, ElhagyottSor),
+    ( 
+        member([], ElhagyottSor) -> MxKi = [] 
+    ;
+        elhagy_kiveve(E, SorIx, Oszlop, ElhagyottOszlop),
+        ( 
+            member([], ElhagyottOszlop) -> MxKi = [] 
+        ;
+            cserel_lista_elem(ElhagyottOszlop, SorIx, E, CsereltOszlop),
+            cserel_sor(MxBe, SorIx, ElhagyottSor, MxSorCserelt),
+            cserel_oszlop(MxSorCserelt, OszlopIx, CsereltOszlop, MxKi)
+        )
+    ).
+
+szukites_nullas(ElvartZ, SorIx, OszlopIx, Vonal, Tipus, MxBe, MxKi, Sz) :-
+    felvehetik(Vonal, 0, Felvehetik),
+    length(Felvehetik, FelvehetiDb),
+    ( 
+        FelvehetiDb < ElvartZ -> 
+            MxKi = [],
+            Sz = nem
+    ;
+        FelvehetiDb =:= ElvartZ -> 
+            nullat_felveheto_tartomanyok_csere(Vonal, CsereltVonal),
+            (
+                Tipus = oszlop ->
+                    cserel_oszlop(MxBe, OszlopIx, CsereltVonal, MxKi),
+                    Sz = oszl(OszlopIx, 0)
+            ;
+                Tipus = sor ->
+                    cserel_sor(MxBe, SorIx, CsereltVonal, MxKi),
+                    Sz = sor(SorIx, 0)
+            )
+    ).
+
+nullat_felveheto_tartomanyok_csere([], []).
+nullat_felveheto_tartomanyok_csere([H|T1], [[0]|T2]) :-
+    is_list(H),
+    felveheti(H, 0),
+    !,
+    nullat_felveheto_tartomanyok_csere(T1, T2).
+nullat_felveheto_tartomanyok_csere([H|T1], [H|T2]) :-
+    nullat_felveheto_tartomanyok_csere(T1, T2).
+
+% MxKi egy mátrix ahol az adott Sor-t kicseréltük.
+cserel_sor(Matrix, 1, Sor, [Sor|T]) :-  
+    Matrix = [_|T].
+cserel_sor([H|T], Index, Sor, [H|R]) :-
+    Index > 1,
+    NewIndex is Index - 1,
+    cserel_sor(T, NewIndex, Sor, R).
+
+% MxKi egy mátrix ahol az adott Oszlopo-t kicseréltük.
+cserel_oszlop([], _, [], []).  % ha nincs több sor, kész
+cserel_oszlop([Sor|T], Index, [Elem|VegsoOszlopT], [UjSor|UjMatrixT]) :-
+    cserel_lista_elem(Sor, Index, Elem, UjSor),
+    cserel_oszlop(T, Index, VegsoOszlopT, UjMatrixT).
+
+% Kimeneti lista egy olyan lista amiben az Index-el adott helyen kicseréltünk egy elemet.
+cserel_lista_elem([_|T], 1, Elem, [Elem|T]).
+cserel_lista_elem([H|T], Index, Elem, [H|R]) :-
+    Index > 1,
+    NewIndex is Index - 1,
+    cserel_lista_elem(T, NewIndex, Elem, R).
+
+% :- pred kizarasos_szukites(feladvany_leiro::in, t_matrix::in, t_matrix::out, szukites::out).
+
+kizarasos_szukites(szt(N, M, _), MxBe, MxKi, Sz) :- 
+    Z is N - M,
+    vonal_keres(N, M, MxBe, SorIx, OszlopIx, Tipus, E, Vonal),
+    szukites(E, Z, SorIx, OszlopIx, Vonal, Tipus, MxBe, MxKi, Sz).
